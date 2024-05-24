121376e7d153c7312128a41fd0c73089d895bd57
Santiago Mariani <santi@gospartanburg.com>
2024-05-24 17:11:40 -0400

    Start of 8-ball assignment

---
+++ magic_8.js
@@ -0,0 +1,17 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    const theQuestion = document.querySelector('#theQuestion');
+
+    myForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const name = formData.get('theAnswer');
+
+        const showName = document.querySelector('#showName');
+        showName.innerText = name;
+    })
+
+})
