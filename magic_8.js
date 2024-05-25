af6480bb4388a3a3706cfb639ee6017352978bb3
Santiago Mariani <santi@gospartanburg.com>
2024-05-25 13:30:42 -0400

    Button working

--- magic_8.js
+++ magic_8.js
@@ -1,17 +1,22 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function (event) {
-
     const theQuestion = document.querySelector('#theQuestion');
+    const display8 = document.querySelector('#display8');
+    const showName = document.querySelector('#showName');
 
-    theQuestion.addEventListener('submit', function (event) {
+    function showButton(event) {
         event.preventDefault();
-
+        display8.style.display = 'none';
         const formData = new FormData(event.target);
-        const name = formData.get('theAnswer');
-
-        const showName = document.querySelector('#showName');
+        const name = formData.get('userInput');
         showName.innerText = name;
-    })
+    }
 
-})
+    theQuestion.addEventListener('submit', showButton);
+
+    document.getElementById('submitButton').addEventListener('click', function(event) {
+        theQuestion.dispatchEvent(new Event('submit')); 
+        submitButton.style.display = 'none';
+    });
+});
