52aaa75ade8373a0013391828e9889acbe1b1b8d
Santiago Mariani <santi@gospartanburg.com>
2024-05-25 14:10:07 -0400

    Working copy

--- magic_8.js
+++ magic_8.js
@@ -8,10 +8,11 @@ document.addEventListener('DOMContentLoaded', function (event) {
     function showButton(event) {
         event.preventDefault();
         display8.style.display = 'none';
-        const formData = new FormData(event.target);
-        const name = formData.get('userInput');
-        showName.innerText = name;
-    }
+        const allAnswers = ["6*7","7*6","√(42^2)","3!+6","8.4*5","2^21","√(3^6)","84/2","2^5.39","√(3^3.57)","√(10^3)","√(5!+16)","e^ln42","√(φ^3-1)","sinh42","√(2^21)","84/2","√(10^3)","√(5!+16)","e^ln42","√(6^2-6)","cos42","42^(2/2)","√(3^2*2)","φ*26","cos0*42","42^(2/2)","√(3^2*2)","φ*26","sinh42","√(2^21)","84/2","7*6","√(2^21)","84/2","7*6","√(2^21)","84/2","7*6","√(2^21)","84/2","7*6"];
+        const randomizedAnswers = Math.floor(Math.random() * allAnswers.length);
+        const actualAnswer = allAnswers[randomizedAnswers];
+        showName.innerText = actualAnswer;
+        };
 
     theQuestion.addEventListener('submit', showButton);
 
